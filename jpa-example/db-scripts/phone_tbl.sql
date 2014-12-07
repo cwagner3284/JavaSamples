be315409be0a84e2f3176dab8f921cca7c70fe52
sayembd <sayem64@gmail.com>
2014-12-07 15:52:31 +0900

    Added drop table syntax for phone.

--- jpa-example/db-scripts/phone_tbl.sql
+++ jpa-example/db-scripts/phone_tbl.sql
@@ -1,10 +1,14 @@
-﻿CREATE 
+﻿DROP TABLE
+	phone
+;
+
+CREATE 
 TABLE	phone
 	(
 		id bigint NOT NULL,
 		type character varying(20) NOT NULL,
-		phone_number numeric(15) NOT NULL,
-		country_code int NOT NULL,
+		phone_number character varying(15) NOT NULL,
+		country_code character varying(5) NOT NULL,
 		owner_id bigint NOT NULL,
 		CONSTRAINT phone_pkey PRIMARY KEY (id),
 		CONSTRAINT owner_fkey FOREIGN KEY (owner_id)
